f202b465e4867d84eb3649cd425d841417e06cb5
samuellee-zy <samuelee.zy@gmail.com>
2022-09-15 11:42:03 +1000

    Updated storage account name

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "this" {
 }
 
 resource "azurerm_storage_account" "this" {
-  name                     = "samstorageaccount"
+  name                     = "birthdayboysamherehello"
   resource_group_name      = azurerm_resource_group.this.name
   location                 = azurerm_resource_group.this.location
   account_tier             = "Standard"
